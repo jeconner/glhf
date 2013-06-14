aa34901138a455392e12213b8ac5c6d65df7fa10
Jerry Conner <jerry.conner@gmail.com>
2013-06-14 19:25:57 -0400

    hello world

---
+++ glhf/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+//--- OpenGL ---
+#include <GL/glew.h>
+#include <GL/wglew.h>
+#include <GL/glut.h>
+//--------------
+
+using namespace std;
+
+int main(int argc, char* argv) {
+	cout << "Hello, World!";
+
+	getchar();
+	return 0;
+}
