807dee0cfd7f5d77df043ee9cbe90fb409d2177e
Jerry Conner <jerry.conner@gmail.com>
2013-06-14 20:55:04 -0400

    first opengl 3.3 window

--- glhf/main.cpp
+++ glhf/main.cpp
@@ -1,16 +1,79 @@
-#include <iostream>
-
-//--- OpenGL ---
+// http://openglbook.com/the-book/chapter-1-getting-started/
+//
 #include <GL/glew.h>
-#include <GL/wglew.h>
-#include <GL/glut.h>
-//--------------
+#include <GL/freeglut.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define WINDOW_TITLE_PREFIX "Chapter 1"
 
 using namespace std;
 
-int main(int argc, char* argv) {
-	cout << "Hello, World!";
+int CurrentWidth = 800,
+	CurrentHeight = 600,
+	WindowHandle = 0;
+
+void Initialize(int, char*[]);
+void InitWindow(int, char*[]);
+void ResizeFunction(int, int);
+void RenderFunction(void);
+
+int main(int argc, char* argv[])
+{
+    Initialize(argc, argv);
+
+    glutMainLoop();
 
 	getchar();
-	return 0;
+    exit(EXIT_SUCCESS);
+}
+
+void Initialize(int argc, char* argv[])
+{
+	InitWindow(argc, argv);
+
+	fprintf(stdout, "INFO: OpenGL Version: %s\n", glGetString(GL_VERSION));
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+void InitWindow(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+
+	glutInitContextVersion(3.3, 0);
+	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+
+	glutInitWindowSize(CurrentWidth, CurrentHeight);
+
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+
+	WindowHandle = glutCreateWindow(WINDOW_TITLE_PREFIX);
+
+	if (WindowHandle < 1) {
+		fprintf(stderr, "ERROR: Could not create a new rendering window.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	glutReshapeFunc(ResizeFunction);
+	glutDisplayFunc(RenderFunction);
+}
+
+void ResizeFunction(int Width, int Height)
+{
+	CurrentWidth = Width;
+	CurrentHeight = Height;
+	glViewport(0, 0, CurrentWidth, CurrentHeight);
+}
+
+void RenderFunction(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glutSwapBuffers();
+	glutPostRedisplay();
 }
